1655a35dd8b0a861427f36a7468059985cf66e4e
liviafonseca <liviagcf@gmail.com>
2019-06-20 22:29:17 -0300

    Implementada a cache com verificação de tempo de validade

--- trabalho1.py
+++ trabalho1.py
@@ -1,11 +1,15 @@
 import socket
 import thread
 import string
+import time
+import os
+import os.path
 
 
 MAX_CONNECTIONS = 50
 MAX_DATA = 4000
 PROXY_PORT = 8002
+TIME = 31536000
 
 def findWebserver (request):
 	# Divide a mensagem em linhas
@@ -85,6 +89,52 @@ def sendBlacklistMessage(conn):
 	response = file.read()
 	conn.send(response)
 
+def fileFromServer(request, conn, webserver, port, url):
+	s_proxy_webserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s_proxy_webserver.connect((webserver, port))
+	s_proxy_webserver.send(request)         
+
+	
+	while 1:
+		data = s_proxy_webserver.recv(MAX_DATA)
+		
+		if (len(data) > 0):
+			conn.send(data)
+			saveCache(url, data)
+		else:
+			break
+	s_proxy_webserver.close()
+	conn.close()
+
+
+def saveCache(url, content):
+	filename = url.replace('/', '_')
+	cached_file = open('cache/' + filename, 'a+')
+	cached_file.write(content)
+	cached_file.close()
+
+
+def getData(request, conn, webserver, port, url):
+	filename = url.replace('/', '_')
+	
+	try:
+		file = open('cache/' + filename)
+
+		if( (time.time() - os.path.getmtime('cache/' + filename)) < TIME ):
+			data = file.read()
+			conn.send(data)
+
+			conn.close()
+		else:
+
+			fileFromServer(request, conn, webserver, port, url)
+
+	except IOError:
+		fileFromServer(request, conn, webserver, port, url)
+
+
+
+
 def  manageRequest (conn, client_addr, contador):
 	# Mensagem HTTP
 	request = conn.recv(MAX_DATA)
@@ -92,7 +142,7 @@ def  manageRequest (conn, client_addr, contador):
 	webserver, port = findWebserver(request)
 	url = findURL(request)
 
-	print(webserver, port)
+	print(webserver, port, url)
 
 	isBlack = verifyBlacklist(webserver)
 	if(isBlack):
@@ -105,20 +155,7 @@ def  manageRequest (conn, client_addr, contador):
 	isWhite = verifyWhitelist(webserver)
 	if(isWhite):
 		try:
-			s_proxy_webserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s_proxy_webserver.connect((webserver, port))
-			s_proxy_webserver.send(request)         
-
-			
-			while 1:
-				data = s_proxy_webserver.recv(MAX_DATA)
-				
-				if (len(data) > 0):
-					conn.send(data)
-				else:
-					break
-			s_proxy_webserver.close()
-			conn.close()
+			getData(request, conn, webserver, port, url)
 			log(url, port, "LOG: Site permitido (Whitelist)")
 
 		except socket.error, (value, message):
